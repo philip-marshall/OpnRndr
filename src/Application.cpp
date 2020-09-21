31bcfece852e3efb94f8d8d4cb66725ee06847f2
philip-marshall <philip-marshall@users.noreply.github.com>
2020-09-20 19:59:55 -0600

    Created Buffer Layout class and Shader class for abstraction

--- src/Application.cpp
+++ src/Application.cpp
@@ -7,97 +7,11 @@
 #include <sstream>
 
 #include "Renderer.h"
+
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
-
-// Used to return two strings from ParseShader
-struct ShaderProgramSource
-{
-    std::string VertexSource;
-    std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath)
-{
-    std::ifstream stream(filepath);
-
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-
-    std::string line;
-
-    // using a stack allocated array
-    std::stringstream ss[2];
-
-    ShaderType type = ShaderType::NONE;
-
-    while (getline(stream, line))
-    {
-        // Checks to make sure it is the beginning of a shader
-        if (line.find("#shader") != std::string::npos)
-        {
-            // This will run if the shader code is for a vertex shader
-            if (line.find("vertex") != std::string::npos)
-                type = ShaderType::VERTEX;
-
-                // This will run if the code is a fragment shader
-            else if (line.find("fragment") != std::string::npos)
-                type = ShaderType::FRAGMENT;
-        }
-
-        // If this is any other type of code, add it to the appropriate shader
-        else
-        {
-            ss[(int)type] << line << '\n';
-        }
-    }
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    // Error handling code
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char)); // alloca lets you allocate memory on the stack rather than doing it with "char* varName = new char[length]"
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-
-    return id;
-}
-
-// This will hold the code required to compile the vertext and fragment shaders
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-}
+#include "VertexArray.h"
+#include "Shader.h"
 
 int main(void)
 {
@@ -142,40 +56,26 @@ int main(void)
             0, 1, 2,
             2, 3, 0
         };
-
-        // Create vertex array attribute for core profile
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         // index buffer object
         IndexBuffer ib(indices, 6);
 
-        // Read in shaders and parse to appropriate string, display to console information for debugging
-        ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-        std::cout << "VERTEX" << std::endl;
-        std::cout << source.VertexSource << std::endl;
-        std::cout << "FRAGMENT" << std::endl;
-        std::cout << source.FragmentSource << std::endl;
-
-        // Apply source struct to CreateShader with appropriate members
-        unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-        GLCall(glUseProgram(shader));
-
-        GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-        ASSERT(location != -1);
-        GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
         // Unbind everything
-        GLCall(glBindVertexArray(0));
-        GLCall(glUseProgram(0));
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -186,13 +86,13 @@ int main(void)
             glClear(GL_COLOR_BUFFER_BIT);
 
             // Bind shader
-            GLCall(glUseProgram(shader));
+            shader.Bind();
 
             // Animate color change in call loop
-            GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
             // bind vertex array and index buffer
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             // Branch to change color based on current red value
@@ -212,7 +112,6 @@ int main(void)
             /* Poll for and process events */
             glfwPollEvents();
         }
-        GLCall(glDeleteProgram(shader));
     }
 
     glfwTerminate();
---
+++ src/Shader.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+// Used to return two strings from ParseShader
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererID;
+	// caching for uniforms
+	std::unordered_map<std::string, int> m_UniformLocationChache;
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	// Set uniforms
+	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
+private:
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	unsigned int GetUniformLocation(const std::string& name);
+};
