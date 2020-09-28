d590fea8b9fb89c68c45e911d9bcc164fa759ffe
philip-marshall <philip-marshall@users.noreply.github.com>
2020-09-27 20:54:31 -0600

    Added texture loading and texture coordinates for our box

--- src/Application.cpp
+++ src/Application.cpp
@@ -9,9 +9,11 @@
 #include "Renderer.h"
 
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -46,10 +48,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-            -0.5f, -0.5,
-             0.5f, -0.5f,
-             0.5f, 0.5f,
-            -0.5f, 0.5f,
+            -0.5f, -0.5, 0.0f, 0.0f, // 0
+             0.5f, -0.5f, 1.0f, 0.0f, // 1
+             0.5f, 0.5f, 1.0f, 1.0f, // 2
+            -0.5f, 0.5f, 0.0f, 1.0f // 3
         };
 
         unsigned int indices[] = {
@@ -57,10 +59,14 @@ int main(void)
             2, 3, 0
         };
         
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -71,29 +77,30 @@ int main(void)
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
+        Texture texture("res/textures/Capture.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
+
         // Unbind everything
         va.Unbind();
         vb.Unbind();
         ib.Unbind();
         shader.Unbind();
 
+        Renderer renderer;
+
         float r = 0.0f;
         float increment = 0.05f;
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
-            glClear(GL_COLOR_BUFFER_BIT);
+            renderer.Clear();
 
             // Bind shader
             shader.Bind();
 
             // Animate color change in call loop
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
-            // bind vertex array and index buffer
-            va.Bind();
-            ib.Bind();
 
             // Branch to change color based on current red value
             if (r > 1.0f)
@@ -104,7 +111,7 @@ int main(void)
             r += increment;
 
             // Function is wrapped in GLCall macro which will empty the error buffer and then check for errors after the function call
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
--- src/Shader.h
+++ src/Shader.h
@@ -25,11 +25,12 @@ public:
 	void Unbind() const;
 
 	// Set uniforms
+	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 };
